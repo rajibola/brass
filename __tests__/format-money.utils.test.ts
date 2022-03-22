6e2bdbd4af191ef5edebd0858735b6fd56958f85
Ridwan Ajibola <51078424+rajibola@users.noreply.github.com>
2022-03-22 22:08:10 +0100

    test: Completed test for format money function

---
+++ __tests__/format-money.utils.test.ts
@@ -0,0 +1,29 @@
+import React from 'react';
+import {formatMoney} from 'utils';
+
+describe('formatMoney', () => {
+  it('should format money', () => {
+    expect(formatMoney(1000)).toEqual('₦ 1,000.00');
+    expect(formatMoney(1000, 'USD')).toEqual('$ 1,000.00');
+    expect(formatMoney(1000, 'GBP')).toEqual('£ 1,000.00');
+    expect(formatMoney(1000, 'EUR')).toEqual('€ 1,000.00');
+    expect(formatMoney(1000, 'JPY')).toEqual('¥ 1,000.00');
+    expect(formatMoney(1000, 'CAD')).toEqual('$ 1,000.00');
+    expect(formatMoney(1000, 'AUD')).toEqual('$ 1,000.00');
+
+    expect(formatMoney(1000, 'SGD')).toEqual('$ 1,000.00');
+    expect(formatMoney(1000, 'ZAR')).toEqual('R 1,000.00');
+  });
+
+  it('should format money with decimals', () => {
+    expect(formatMoney(1000, 'USD', 2)).toEqual('$ 1,000.00');
+    expect(formatMoney(1000, 'GBP', 2)).toEqual('£ 1,000.00');
+    expect(formatMoney(1000, 'EUR', 2)).toEqual('€ 1,000.00');
+    expect(formatMoney(1000, 'JPY', 2)).toEqual('¥ 1,000.00');
+    expect(formatMoney(1000, 'CAD', 2)).toEqual('$ 1,000.00');
+    expect(formatMoney(1000, 'AUD', 2)).toEqual('$ 1,000.00');
+
+    expect(formatMoney(1000, 'SGD', 2)).toEqual('$ 1,000.00');
+    expect(formatMoney(1000, 'ZAR', 2)).toEqual('R 1,000.00');
+  });
+});
