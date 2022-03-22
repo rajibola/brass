a226e474349b8743807235d2563ad350d9400473
Ridwan Ajibola <51078424+rajibola@users.noreply.github.com>
2022-03-22 01:21:08 +0100

    feat: Created new components

--- src/screens/payments-history/index.tsx
+++ src/screens/payments-history/index.tsx
@@ -1,11 +1,12 @@
 import {useSearch} from 'hooks';
+import moment from 'moment';
 import React, {FC, useEffect, useState} from 'react';
 import {FlatList, Text, View} from 'react-native';
 import {useDispatch, useSelector} from 'react-redux';
 import {ITransferHistory} from 'redux';
 import {RootDispatch, RootState} from 'redux/store';
 import {ModalView, SearchBar, TransactionList} from 'shared';
-import {deviceHeight} from 'utils';
+import {deviceHeight, formatMoney, hp, wp} from 'utils';
 import {styles} from './styles';
 
 export const PaymentHistory = () => {
@@ -70,10 +71,69 @@ export const Preview: FC<{
       <View
         style={{
           flex: 1,
+          paddingHorizontal: wp(25),
         }}>
-        <Text style={{fontSize: 18}}>{data?.amount}</Text>
+        <Text
+          style={{
+            textAlign: 'center',
+            fontSize: 16,
+            lineHeight: 28,
+            fontWeight: 'bold',
+            marginBottom: hp(20),
+          }}>
+          Fund transfer from Ridwan Ajibola to {data?.recipient.name}
+        </Text>
+        <TwoItemList properties="Amount" value={formatMoney(data?.amount)} />
+        <TwoItemList
+          properties="Transaction Date"
+          value={moment(data?.createdAt).format('lll')}
+        />
+        <TwoItemList properties="Sender" value={'Ridwan Ajibola'} />
+        <TwoItemList properties="Reference Number" value={data?.reference} />
+        <TwoItemList properties="Receiver" value={data?.recipient.name} />
+        <TwoItemList
+          properties="Account Number"
+          value={data?.recipient.details.account_number}
+        />
+        <TwoItemList
+          properties="Receiving Bank"
+          value={data?.recipient.details.bank_name}
+        />
+
+        <View style={{borderTopWidth: 1, marginTop: hp(20)}}>
+          <Text
+            style={{
+              paddingTop: hp(20),
+            }}>
+            Disclaimer: Lorem ipsum dolor sit amet consectetur adipisicing elit.
+            Quas iusto consequatur consequuntur placeat recusandae minus iste
+            autem quod fugit architecto. Consequuntur nihil soluta, illum minima
+            reiciendis ducimus aspernatur beatae reprehenderit fuga ullam
+            incidunt deleniti? Natus aliquid nostrum minus dignissimos adipisci.
+          </Text>
+        </View>
       </View>
     </ModalView>
+  );
+};
+
+export const TwoItemList: FC<{properties: string; value: string | number}> = ({
+  properties,
+  value,
+}) => {
+  return (
+    <View
+      style={{
+        // flex: 1,
+        flexDirection: 'row',
+        justifyContent: 'space-between',
+        alignItems: 'center',
+        height: hp(50),
+        width: '100%',
+      }}>
+      <Text style={{fontSize: 16, fontWeight: '300'}}>{properties}: </Text>
+      <Text style={{fontSize: 16}}>{value}</Text>
+    </View>
   );
 };
 
